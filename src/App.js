2bfb66ea6c0f834a9ec27ccf7e2a9d56f3144610
edmundsamina <edmundsamina@gmail.com>
2023-01-07 16:09:03 +0000

    beautiful assymetric work of art

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import SpoonColumn from './components/spoonColumn/spoonColumn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Lani is the best</h1>
+    <SpoonColumn/>
     </div>
   );
 }
--- src/components/spoonColumn/spoonColumn.js
+++ src/components/spoonColumn/spoonColumn.js
@@ -1,1 +1,16 @@
 //this will be the spoon column component
+import './spoonColumn.css'
+
+export default function SpoonColumn(props){
+    return(
+        <div className="column-container">
+            <div className="spoon-number">
+                <img src={props.img} alt="spoons"/>
+            </div>
+            <div className="cards-container">
+            cards go here
+            </div>
+        </div>
+    )
+
+}
